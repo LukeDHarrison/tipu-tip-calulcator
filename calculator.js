90e98d4492fa0eea4eb5cfac747888f3adb70758
Luke D Harrison <lukedharrison19@gmail.com>
2021-12-06 12:11:28 +0000

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,102 @@
+//From element
+const form = document.getElementById("inputForm");
+
+//Percentage Buttons
+const percentageButtons = document.querySelectorAll("percentage");
+const tenPercentBtn = document.getElementById("tenPercentBtn");
+const fifteenPercentBtn = document.getElementById("fifteenPercentBtn");
+const twentyPercentBtn = document.getElementById("twentyPercentBtn");
+const twentyFivePercentBtn = document.getElementById("twentyFivePercentBtn");
+
+//Highlight
+const highlight = document.getElementById("highlight");
+
+// Split Bill Button
+const split = document.getElementById("split");
+
+//Outputs
+const tipOutput = document.getElementById("tipOutput");
+const totalBillOutput = document.getElementById("totalBillOutput");
+
+// Calculate percentage of bill input
+
+let bill;
+let updateTip;
+let billTotal;
+
+tenPercentBtn.addEventListener("click", function () {
+  bill = document.getElementById("billInput").value;
+  const tenPercent = (10 / 100) * Number(bill);
+  updateTip = Math.round((tenPercent + Number.EPSILON) * 100) / 100;
+
+  if (bill) {
+    tipOutput.innerHTML = updateTip;
+    highlight.classList.add("green");
+  } else {
+    tipOutput.innerHTML = 0;
+    highlight.classList.remove("green");
+    form.classList.toggle("user-prompt");
+  }
+
+  billTotal = Number(bill) + tenPercent;
+  totalBillOutput.innerHTML = billTotal;
+});
+
+fifteenPercentBtn.addEventListener("click", function () {
+  bill = document.getElementById("billInput").value;
+  const fifteenPercent = (15 / 100) * Number(bill);
+  updateTip = Math.round((fifteenPercent + Number.EPSILON) * 100) / 100;
+
+  if (bill) {
+    tipOutput.innerHTML = updateTip;
+    highlight.classList.add("green");
+  } else {
+    tipOutput.innerHTML = 0;
+    highlight.classList.remove("green");
+    form.classList.toggle("user-prompt");
+  }
+
+  billTotal = Number(bill) + fifteenPercent;
+  totalBillOutput.innerHTML = billTotal;
+});
+
+twentyPercentBtn.addEventListener("click", function () {
+  bill = document.getElementById("billInput").value;
+  const twentyPercent = (20 / 100) * Number(bill);
+  updateTip = Math.round((twentyPercent + Number.EPSILON) * 100) / 100;
+
+  if (bill) {
+    tipOutput.innerHTML = updateTip;
+    highlight.classList.add("green");
+  } else {
+    tipOutput.innerHTML = 0;
+    highlight.classList.remove("green");
+    form.classList.toggle("user-prompt");
+  }
+
+  billTotal = Number(bill) + twentyPercent;
+  totalBillOutput.innerHTML = billTotal;
+});
+
+twentyFivePercentBtn.addEventListener("click", function () {
+  bill = document.getElementById("billInput").value;
+  const twentyFivePercent = (25 / 100) * Number(bill);
+  updateTip = Math.round((twentyFivePercent + Number.EPSILON) * 100) / 100;
+
+  if (bill) {
+    tipOutput.innerHTML = updateTip;
+    highlight.classList.add("green");
+  } else {
+    tipOutput.innerHTML = 0;
+    highlight.classList.remove("green");
+    form.classList.toggle("user-prompt");
+  }
+
+  billTotal = Number(bill) + twentyFivePercent;
+  totalBillOutput.innerHTML = billTotal;
+});
+
+split.addEventListener("click", function () {
+  const splitBill = billTotal / 2;
+  totalBillOutput.innerHTML = splitBill + " each";
+});
